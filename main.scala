894a71249e029373d690e3609ec9cc5cb920e414
hampu <hampus.eden@gmail.com>
2024-07-26 00:06:47 +0200

    copy more examples from the doodle website

--- main.scala
+++ main.scala
@@ -1,12 +1,9 @@
 //> using dep org.creativescala::doodle::0.22.0
 
-// The "Image" DSL is the easiest way to create images
-import doodle.image.*
 // Colors and other useful stuff
 import doodle.core.*
-
 // Extension methods
-import doodle.image.syntax.all.*
+import doodle.syntax.all.*
 // Render to a window using Java2D (must be running in the JVM)
 import doodle.java2d.*
 // Need the Cats Effect runtime to run everything
@@ -14,8 +11,14 @@ import cats.effect.unsafe.implicits.global
 
 @main
 def main(): Unit =
-  val blackSquare = Image.rectangle(30, 30).fillColor(Color.black)
-  val redSquare = Image.rectangle(30, 30).fillColor(Color.red)
+  val pic =
+    layoutExample()
+
+  pic.draw()
+
+def basicExample(): Picture[?] =
+  val blackSquare = Picture.rectangle(30, 30).fillColor(Color.black)
+  val redSquare = Picture.rectangle(30, 30).fillColor(Color.red)
 
   val twoByTwo = 
     (redSquare beside blackSquare) above (blackSquare beside redSquare)
@@ -26,4 +29,53 @@ def main(): Unit =
   val chessboard =
     (fourByFour beside fourByFour) above (fourByFour beside fourByFour)
 
-  chessboard.draw()
+  val basicShapes = 
+    Picture
+      .circle(200)
+      .strokeColor(Color.white)
+      .on(Picture.square(125).strokeColor(Color.white))
+      .on(Picture.triangle(100, 100).strokeColor(Color.white))
+      .strokeWidth(5.0)
+
+  val feather =
+    ClosedPath.empty
+      .lineTo(100, 100)
+      .curveTo(90, 75, 90, 25, 10, 10)
+      .moveTo(100, 100)
+      .curveTo(75, 90, 25, 90, 10, 10)
+      .path
+      .strokeWidth(5.0)
+
+  val sinePoints =
+    for (x <- 0 to 360) yield Point(x, x.degrees.sin * 100)
+  val sineCurve = Picture.interpolatingSpline(sinePoints).debug
+
+  // (basicShapes on chessboard).beside(feather).above(sineCurve)
+  sineCurve
+
+def layoutExample(): Picture[?] =
+  val debugLayout =
+    Picture
+      .circle(100)
+      .debug
+      .beside(Picture.regularPolygon(5, 30).debug)
+      .above(
+        Picture.circle(100).beside(Picture.regularPolygon(5, 30)).debug
+      )
+  
+  val atAndOriginAt =
+    Picture.circle(100).at(25, 25).debug
+      .beside(Picture.circle(100).originAt(25, 25).debug)
+
+  val pentagon =
+    val circ = Picture.circle(10)
+    circ.at(50, 0.degrees)
+      .on(circ.at(50, 72.degrees))
+      .on(circ.at(50, 144.degrees))
+      .on(circ.at(50, 216.degrees))
+      .on(circ.at(50, 288.degrees))
+    
+  debugLayout
+    .above(atAndOriginAt)
+    .beside(pentagon)
+    .scale(2, 2)
